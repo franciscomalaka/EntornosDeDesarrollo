41718d307622d6326eb5235d962b515fd3b6e90b
fran <al.francisco.garate.soto@iesportada.org>
2024-05-16 18:06:38 +0200

    nueva rama

---
+++ Proyecto1/src/PaqueteGitExamen/Rama.java
@@ -0,0 +1,13 @@
+package PaqueteGitExamen;
+
+public class Rama {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Mensaje1");
+System.out.println("Mensaje2");
+
+	}
+
+}
